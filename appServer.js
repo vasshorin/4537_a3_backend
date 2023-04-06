a0441a573d6d289d3fffaed3d5a109943bfd7ba9
vasshorin <)tSz%gBWz1>
2023-04-06 16:01:23 -0700

    Change place for middleware

--- appServer.js
+++ appServer.js
@@ -320,150 +320,6 @@ app.post(
   })
 );
 
-// module.exports = { authApp: app, authStart: start() };
-
-
-// const start = asyncWrapper(async () => {
-
-
-//   app.listen(6003, (err) => {
-//     if (err)
-//       throw new PokemonDbError(err)
-//     else
-//       console.log(`Phew! Server is running on port: ${process.env.pokeServerPORT}`);
-//   })
-// })
-
-const authUser = asyncWrapper(async (req, res, next) => {
-  // const token = req.body.appid
-  const token = req.header('auth-token-access')
-
-  if (!token) {
-    // throw new PokemonAuthError("No Token: Please provide an appid query parameter.")
-    throw new PokemonAuthError("No Token: Please provide the access token using the headers.")
-  }
-  try {
-    const verified = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-    next()
-  } catch (err) {
-    throw new PokemonAuthError("Invalid Token Verification. Log in again.")
-  }
-})
-
-
-const authAdmin = asyncWrapper(async (req, res, next) => {
-  const payload = jwt.verify(req.header('auth-token-access'), process.env.ACCESS_TOKEN_SECRET)
-  if (payload?.user?.role == "admin") {
-    return next()
-  }
-  throw new PokemonAuthError("Access denied")
-})
-
-
-app.use(authUser) // Boom! All routes below this line are protected
-app.get('/api/v1/pokemons', asyncWrapper(async (req, res) => {
-  if (!req.query["count"])
-    req.query["count"] = 10
-  if (!req.query["after"])
-    req.query["after"] = 0
-  // try {
-  const docs = await pokeModel.find({})
-    .sort({ "id": 1 })
-    .skip(req.query["after"])
-    .limit(req.query["count"])
-  res.json(docs)
-  // } catch (err) { res.json(handleErr(err)) }
-}))
-
-app.get('/api/v1/pokemon', asyncWrapper(async (req, res) => {
-  // try {
-  const { id } = req.query
-  const docs = await pokeModel.find({ "id": id })
-  if (docs.length != 0) res.json(docs)
-  else res.json({ errMsg: "Pokemon not found" })
-  // } catch (err) { res.json(handleErr(err)) }
-}))
-
-app.use(authAdmin)
-app.post('/api/v1/pokemon/', asyncWrapper(async (req, res) => {
-  // try {
-  console.log(req.body);
-  if (!req.body.id) throw new PokemonBadRequestMissingID()
-  const poke = await pokeModel.find({ "id": req.body.id })
-  if (poke.length != 0) throw new PokemonDuplicateError()
-  const pokeDoc = await pokeModel.create(req.body)
-  res.json({
-    msg: "Added Successfully"
-  })
-  // } catch (err) { res.json(handleErr(err)) }
-}))
-
-app.delete('/api/v1/pokemon', asyncWrapper(async (req, res) => {
-  // try {
-  const docs = await pokeModel.findOneAndRemove({ id: req.query.id })
-  if (docs)
-    res.json({
-      msg: "Deleted Successfully"
-    })
-  else
-    // res.json({ errMsg: "Pokemon not found" })
-    throw new PokemonNotFoundError("");
-  // } catch (err) { res.json(handleErr(err)) }
-}))
-
-  app.get('/users/me', authUser, asyncWrapper(async (req, res) => {
-    const user = req.user;
-    res.send(user);
-  }));
-  
-app.put('/api/v1/pokemon/:id', asyncWrapper(async (req, res) => {
-  // try {
-  const selection = { id: req.params.id }
-  const update = req.body
-  const options = {
-    new: true,
-    runValidators: true,
-    overwrite: true
-  }
-  const doc = await pokeModel.findOneAndUpdate(selection, update, options)
-  // console.log(docs);
-  if (doc) {
-    res.json({
-      msg: "Updated Successfully",
-      pokeInfo: doc
-    })
-  } else {
-    // res.json({ msg: "Not found", })
-    throw new PokemonNotFoundError("");
-  }
-  // } catch (err) { res.json(handleErr(err)) }
-}))
-
-app.patch('/api/v1/pokemon/:id', asyncWrapper(async (req, res) => {
-  // try {
-  const selection = { id: req.params.id }
-  const update = req.body
-  const options = {
-    new: true,
-    runValidators: true
-  }
-  const doc = await pokeModel.findOneAndUpdate(selection, update, options)
-  if (doc) {
-    res.json({
-      msg: "Updated Successfully",
-      pokeInfo: doc
-    })
-  } else {
-    // res.json({  msg: "Not found" })
-    throw new PokemonNotFoundError("");
-  }
-  // } catch (err) { res.json(handleErr(err)) }
-}))
-
-app.get('/report', asyncWrapper(async (req, res) => {
-  
-}))
-
 
 
 app.get('/log/top-api-users', asyncWrapper(async (req, res) => {
@@ -655,6 +511,135 @@ console.log(docs);
   res.send(docs);
 }));
 
+const authUser = asyncWrapper(async (req, res, next) => {
+  // const token = req.body.appid
+  const token = req.header('auth-token-access')
+
+  if (!token) {
+    // throw new PokemonAuthError("No Token: Please provide an appid query parameter.")
+    throw new PokemonAuthError("No Token: Please provide the access token using the headers.")
+  }
+  try {
+    const verified = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    next()
+  } catch (err) {
+    throw new PokemonAuthError("Invalid Token Verification. Log in again.")
+  }
+})
+
+
+const authAdmin = asyncWrapper(async (req, res, next) => {
+  const payload = jwt.verify(req.header('auth-token-access'), process.env.ACCESS_TOKEN_SECRET)
+  if (payload?.user?.role == "admin") {
+    return next()
+  }
+  throw new PokemonAuthError("Access denied")
+})
+
+
+app.use(authUser) // Boom! All routes below this line are protected
+app.get('/api/v1/pokemons', asyncWrapper(async (req, res) => {
+  if (!req.query["count"])
+    req.query["count"] = 10
+  if (!req.query["after"])
+    req.query["after"] = 0
+  // try {
+  const docs = await pokeModel.find({})
+    .sort({ "id": 1 })
+    .skip(req.query["after"])
+    .limit(req.query["count"])
+  res.json(docs)
+  // } catch (err) { res.json(handleErr(err)) }
+}))
+
+app.get('/api/v1/pokemon', asyncWrapper(async (req, res) => {
+  // try {
+  const { id } = req.query
+  const docs = await pokeModel.find({ "id": id })
+  if (docs.length != 0) res.json(docs)
+  else res.json({ errMsg: "Pokemon not found" })
+  // } catch (err) { res.json(handleErr(err)) }
+}))
+
+app.use(authAdmin)
+app.post('/api/v1/pokemon/', asyncWrapper(async (req, res) => {
+  // try {
+  console.log(req.body);
+  if (!req.body.id) throw new PokemonBadRequestMissingID()
+  const poke = await pokeModel.find({ "id": req.body.id })
+  if (poke.length != 0) throw new PokemonDuplicateError()
+  const pokeDoc = await pokeModel.create(req.body)
+  res.json({
+    msg: "Added Successfully"
+  })
+  // } catch (err) { res.json(handleErr(err)) }
+}))
+
+app.delete('/api/v1/pokemon', asyncWrapper(async (req, res) => {
+  // try {
+  const docs = await pokeModel.findOneAndRemove({ id: req.query.id })
+  if (docs)
+    res.json({
+      msg: "Deleted Successfully"
+    })
+  else
+    // res.json({ errMsg: "Pokemon not found" })
+    throw new PokemonNotFoundError("");
+  // } catch (err) { res.json(handleErr(err)) }
+}))
+
+  app.get('/users/me', authUser, asyncWrapper(async (req, res) => {
+    const user = req.user;
+    res.send(user);
+  }));
+  
+app.put('/api/v1/pokemon/:id', asyncWrapper(async (req, res) => {
+  // try {
+  const selection = { id: req.params.id }
+  const update = req.body
+  const options = {
+    new: true,
+    runValidators: true,
+    overwrite: true
+  }
+  const doc = await pokeModel.findOneAndUpdate(selection, update, options)
+  // console.log(docs);
+  if (doc) {
+    res.json({
+      msg: "Updated Successfully",
+      pokeInfo: doc
+    })
+  } else {
+    // res.json({ msg: "Not found", })
+    throw new PokemonNotFoundError("");
+  }
+  // } catch (err) { res.json(handleErr(err)) }
+}))
+
+app.patch('/api/v1/pokemon/:id', asyncWrapper(async (req, res) => {
+  // try {
+  const selection = { id: req.params.id }
+  const update = req.body
+  const options = {
+    new: true,
+    runValidators: true
+  }
+  const doc = await pokeModel.findOneAndUpdate(selection, update, options)
+  if (doc) {
+    res.json({
+      msg: "Updated Successfully",
+      pokeInfo: doc
+    })
+  } else {
+    // res.json({  msg: "Not found" })
+    throw new PokemonNotFoundError("");
+  }
+  // } catch (err) { res.json(handleErr(err)) }
+}))
+
+app.get('/report', asyncWrapper(async (req, res) => {
+  
+}))
 
 
 app.use(handleErr)
